e1509c1be37998f313be453f6d2e72857d569a7d
rizzygoat67 <yoyo.coolestguy@gmail.com>
2026-08-28 10:59:34 +0500

    fix: update catch-all route for production environment in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ app.use("/api/chat", chatRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*splat}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
